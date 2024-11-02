8565914b3184b4f422bed26adeadfa0908b8b28e
margaux <margaux.tarayre@gmail.com>
2024-11-02 02:54:35 +0100

    Add functions link to coins in player

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,11 @@
+import java.util.ArrayList;
+
 public class Player {
     private int x;
     private int y;
     private String name;
     private int coins;
+    private ArrayList<Weapon> inventory;
 
     //Constructeur
     public Player(String name) {
@@ -10,10 +13,11 @@ public class Player {
         this.x = 0;
         this.y = 0;
         this.coins = 80;
+        this.inventory = new ArrayList<>();
     }
     //Getters et setters
     public int getX() {
-        return x;dungeon.displayMap();
+        return x;
     }
     public void setX(int x) {
         this.x = x;
@@ -41,7 +45,29 @@ public class Player {
     public void addCoins(int coins) {
         this.coins += coins;
     }
+    public boolean canAfford(int amount) {
+        return coins >= amount;
+    }
+    public void spendCoins(int amount) {
+        if (canAfford(amount)) {
+            coins -= amount;
+        } else {
+            notEnoughCoins();
+        }
+    }
     public void notEnoughCoins() {
         System.out.println("Vous n'avez pas assez de pièces.");
+    }
+    //armes
+    public void addWeapon(Weapon weapon) {
+        inventory.add(weapon);
+        System.out.println(weapon.getName() + " a été ajouté à votre inventaire.");
+    }
+    public String showInventory() {
+        StringBuilder s = new StringBuilder("Inventaire :\n");
+        for (Weapon w : inventory) {
+            s.append("- ").append(w.toString()).append("\n");
+        }
+        return s.toString();
     }
 }
